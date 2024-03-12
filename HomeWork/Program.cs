2cef70b2f472297917590626720c881103f5437d
Jonas SNK <148264630+Desmer11@users.noreply.github.com>
2024-03-12 12:38:23 +0100

    Add files via upload

---
+++ HomeWork/Program.cs
@@ -0,0 +1,44 @@
+﻿
+    class SumOfEvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.Write("Enter From Number:");
+            int FromNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter To Number:");
+            int ToNumber = Convert.ToInt32(Console.ReadLine());
+            if (FromNumber < ToNumber)
+            {
+                sum = FindSumOfEvenNumbers(sum, FromNumber, ToNumber);
+                Console.Write($"Sum of even numbers from {FromNumber} to {ToNumber} is : {sum}");
+            }
+            else
+            {
+                Console.Write("Invalid From and To Numbers");
+            }
+            Console.ReadLine();
+        }
+        private static int FindSumOfEvenNumbers(int sum, int FromNumber, int ToNumber)
+        {
+            for (int i = FromNumber; i <= ToNumber; i++)
+            {
+                if (i % 2 == 0)
+                    sum += i;
+            }
+            return sum;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
