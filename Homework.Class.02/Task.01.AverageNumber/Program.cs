7557a2fbf040def431a20340ac8e92bcbd520332
Jonas SNK <148264630+Desmer11@users.noreply.github.com>
2024-03-20 23:17:36 +0100

    Add files via upload

---
+++ Homework.Class.02/Task.01.AverageNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿//int array = [10, 15,20,25,30];
+//double average1 = Queryable.Average(array.AsQueryable());
+//Console.WriteLine(average1);
+
+
+
+class AverageNumber
+{  static void Main()
+    {
+        int[] arr = new int[] { 10, 15, 20, 30 };
+        double avg = Queryable.Average(arr.AsQueryable());
+        Console.WriteLine("Average = " + avg);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Homework.Class.02/Task.02.SwapNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿class SwapNumbers
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 8;
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+        (a, b) = (b, a);
+        Console.WriteLine("Now a = " + a);
+        Console.WriteLine("Now b = " + b);
+    }
+}
