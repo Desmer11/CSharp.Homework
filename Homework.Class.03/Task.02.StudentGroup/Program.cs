5d0a5f472c953810daffe2b976a3848cbc120ce6
Jonas SNK <148264630+Desmer11@users.noreply.github.com>
2024-03-20 23:35:05 +0100

    Add files via upload

---
+++ Homework.Class.03/Task.02.StudentGroup/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+string[] studentsG1 = new string[] { "Aka", "Aura", "Kiro", "Gaia", "Ares" };
+
+
+string[] studentsG2 = new string[] { "Caesar", "Marte", "Jones", "Lucas", "Silke" };
+
+
+Console.WriteLine(studentsG1);
+
+Array.ForEach(studentsG1, Console.WriteLine);
+
+Console.WriteLine(String.Join(", ", studentsG2));
+
+
+
+
+
+
+
+
