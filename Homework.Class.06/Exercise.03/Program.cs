c83e5e1d8d707ac59c0abceef2a8cbfd5c746191
Jonas SNK <148264630+Desmer11@users.noreply.github.com>
2024-03-21 11:55:13 +0100

    Commit.Add files via upload

---
+++ Homework.Class.06/Exercise.03/Program.cs
@@ -0,0 +1,28 @@
+﻿// Excercise 03
+Console.WriteLine("\n\nExcercise 03\n");
+string SubString()
+{
+    return "Hello from SEDC CodeAcademy 2024";
+
+}
+
+string methodMessage = SubString();
+
+Console.WriteLine("Write a number");
+string numInput = Console.ReadLine();
+int num;
+bool numparsed = int.TryParse(numInput, out num);
+
+if (numparsed)
+{
+    if (num > methodMessage.Length)
+    {
+        Console.WriteLine("Number Yes");
+    }
+    Console.WriteLine(methodMessage.Substring(0, num));
+
+}
+else
+{
+    Console.WriteLine("Invalid");
+}
