a6f896a73a4b4981090018dbf126a8d24c24271b
Jonas SNK <148264630+Desmer11@users.noreply.github.com>
2024-03-21 11:48:35 +0100

    Add files via upload

---
+++ Homework.Class.08/Task01.StoreNumbersQueue/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string yesNO = "";
+        do
+        {
+            string continueAdding = "Continue adding Numbers to the string (y/n)?: ";
+            Console.Write(continueAdding);
+            yesNO = Console.ReadLine();
+            yesNO = yesNO.ToLower();
+            Numbers();
+
+        } while (yesNO == "y");
+
+        static void Numbers()
+        {
+            Console.WriteLine("Enter your Number: ");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your Number is: " + inputNumber);
+
+
+
+            Queue<int> integerQueue = new Queue<int>(inputNumber);
+            Console.WriteLine("Your Queue is: " + integerQueue);
+
+
+            foreach (int integer in integerQueue)
+            {
+                Console.WriteLine(integer);
+            }
+            Console.WriteLine("Count of Numbers:  " + integerQueue.Count);
+            Console.WriteLine($"Num of members in the queue: {integerQueue.Count}");
+
+
+
+        }
+
+    }
+}
