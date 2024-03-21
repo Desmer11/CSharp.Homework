70419b1a7db72f0cf6a2eff462571bc914bfa3d2
Jonas SNK <148264630+Desmer11@users.noreply.github.com>
2024-03-21 11:41:16 +0100

    Add files via upload

---
+++ Homework.Class.04/Task.01.String from the input/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+using System;
+
+string string1 = "782 675 S826F";
+string greeting = "Welcome to " + string1;
+Console.WriteLine(greeting);
+
+string result1 = string1.Substring(string1.Length - 5);
+Console.WriteLine(result1);
+
+string result2 = string1.Substring(Math.Max(0, string1.Length - 5));
+Console.WriteLine(result2);
+
+
+
+
+
+
+
+
